2e1f0988345454a94034245426d21cb5f2c4b33e
Lamine-br <jl_brahami@esi.dz>
2023-05-10 14:46:30 +0100

    first

---
+++ src/middleware/config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+
+const connectDatabase = () => {
+
+ if(!dotenv.config().error)
+{
+  mongoose.connect("mongodb+srv://jmdif:meryemdif@cluster0.vnstzge.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology:  true,
+  }) 
+    .then((data) => { 
+      console.log(`Mongodb connected with server: `);
+    }).catch((err)=>{
+      console.log(err)
+    });}
+};
+
+module.exports = connectDatabase;
